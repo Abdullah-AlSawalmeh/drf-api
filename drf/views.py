afe4ee216fc5b95be95b800445db9d119f6fc06b
abdullah-alsawalmeh <abdullahalsawalmeh@gmail.com>
2021-07-26 01:23:00 +0300

    first commit

---
+++ drf/views.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Projects
+from .serializer import ProjectsSerializer
+
+# Create your views here.
+class ProjectsListView(generics.ListCreateAPIView):    
+    serializer_class = ProjectsSerializer
+    queryset = Projects.objects.all()
+
+class ProjectsDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProjectsSerializer
+    queryset = Projects.objects.all()
